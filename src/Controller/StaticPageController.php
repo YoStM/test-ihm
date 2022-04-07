54452cfc72d2d0d8b3ac06f93be73f3bc4abd336
YoStM <yo.stm@outlook.com>
2022-04-07 16:43:25 +0200

    test ihm

--- src/Controller/StaticPageController.php
+++ src/Controller/StaticPageController.php
@@ -9,10 +9,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class StaticPageController extends AbstractController
 {
     #[Route('/', name: 'app_landing_page')]
-    public function index(): Response
+    public function landingPage(): Response
     {
-        return $this->render('static_page/index.html.twig', [
-            'controller_name' => 'StaticPageController',
-        ]);
+        return $this->render('static_page/index.html.twig', []);
+    }
+
+    #[Route('/conditions-utilisation', name:'app_terms_conditions')]
+    public function termsCondition(): Response
+    {
+        return $this->render('static_page/terms_condition.html.twig', []);
+    }
+
+    #[Route('/faq', name:'app_faq')]
+    public function faq(): Response
+    {
+        return $this->render('static_page/faq.html.twig', []);
     }
 }
